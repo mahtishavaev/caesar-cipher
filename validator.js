0f20d1b8f55b2c66c5427d9e916ad420b81bede7
Mahti <mahtishavaev@gmail.com>
2021-05-05 14:28:03 +0300

    fix: exit code

--- validator.js
+++ validator.js
@@ -6,15 +6,15 @@ const validateInput = (input) => {
   try {
     fs.accessSync(input, fs.constants.F_OK);
   } catch (error) {
-    console.error(`File ${input} doesn't exists`);
-    process.exit();
+    console.error(`File ${input} doesn't exist`);
+    process.exit(1);
   }
 
   try {
     fs.accessSync(input, fs.constants.R_OK);
   } catch (error) {
     console.error(`Can't read file ${input}`);
-    process.exit();
+    process.exit(1);
   }
 };
 
@@ -24,37 +24,37 @@ const validateOutput = (output) => {
   try {
     fs.accessSync(output, fs.constants.F_OK);
   } catch (error) {
-    console.error(`File ${output} doesn't exists`);
-    process.exit();
+    console.error(`File ${output} doesn't exist`);
+    process.exit(1);
   }
 
   try {
     fs.accessSync(output, fs.constants.W_OK);
   } catch (error) {
     console.error(`Can't write to file ${output}`);
-    process.exit();
+    process.exit(1);
   }
 };
 
 const validateShift = (shift) => {
   if (shift === undefined) {
     console.error(`"shift" is required`);
-    process.exit();
+    process.exit(1);
   }
   if (!Number.isInteger(Number(shift))) {
     console.error(`"shift" must be an integer number`);
-    process.exit();
+    process.exit(1);
   }
 };
 
 const validateAction = (action) => {
   if (action === undefined) {
     console.error(`"action" is required`);
-    process.exit();
+    process.exit(1);
   }
   if (!(action === "encode" || action === "decode")) {
     console.error(`"action" must be either "encode" or "decode"`);
-    process.exit();
+    process.exit(1);
   }
 };
 
